1b9c4840139bc60c8355ce921dccc4346de0f51c
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-09 10:59:12 -0500

    added a product class

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory. 
+    /// </summary>
+    public class Product
+    {
+        private string productName;
+
+        public  string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+        private string productDescription;
+        
+        public string ProductDescription
+        {
+            get { return productDescription; }
+            set { productDescription = value; }
+        }
+        private int productId;
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        public string SayHello()
+        {
+            return "Hello " + ProductName +
+                " (" + ProductId + "): " +
+                ProductDescription;
+        }
+    }
+}

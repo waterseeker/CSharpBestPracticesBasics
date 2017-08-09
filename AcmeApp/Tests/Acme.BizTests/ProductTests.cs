56532e1818b25b9ef3282dc5898e18966945c726
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-09 11:04:49 -0500

    added unit test by clicking inside the method, r-clicking and selecting 'create unit test'

---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.Fail();
+        }
+    }
+}

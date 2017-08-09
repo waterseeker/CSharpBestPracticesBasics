2e8681fb90726c3fc2fa7c953358bbedc24f27a3
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-09 11:34:21 -0500

    added a parameterized constructor that invokes the default constructor by using ': this'

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -11,6 +11,20 @@ namespace Acme.Biz
     /// </summary>
     public class Product
     {
+        public Product()
+        {
+            Console.WriteLine("Product instance created");
+        }
+
+        //using " : this" after the parameterized constructor invokes the default constructor so 
+        //you don't have to repeat any code that is already in the default constructor. 
+        public Product( int productId,
+            string productName,
+            string productDescription) : this()
+        {
+
+        }
+
         private string productName;
 
         public  string ProductName
